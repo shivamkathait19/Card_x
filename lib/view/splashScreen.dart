28fb13c80740b0b47ad1b1d18b0415583fe1a4dc
shivamkathait19 <kathaitshivam151@gmail.com>
2025-01-14 21:36:04 +0530

    Add

--- lib/view/splashScreen.dart
+++ lib/view/splashScreen.dart
@@ -5,14 +5,18 @@ class splashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blueGrey,
       body: Center(
         child: Column(
           children:[
             SizedBox(
               height: 200,
             ),
-            Image.network("https://bsmedia.business-standard.com/_media/bs/img/article/2024-03/12/full/1710223554-0545.jpg",)
-
+            Image.network("https://bsmedia.business-standard.com/_media/bs/img/article/2024-03/12/full/1710223554-0545.jpg",),
+SizedBox(
+  height: 100,
+),
+            Text("MAKE YOUR CARD ",style: TextStyle(fontSize: 20,fontWeight:FontWeight.w200),),
           ],
         ),
       ),
