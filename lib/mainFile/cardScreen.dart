f3a125b2786d05a97d4bc17eec278a8f98470227
shivamkathait19 <kathaitshivam151@gmail.com>
2025-01-20 21:39:40 +0530

    Update cardScreen.dart

--- lib/mainFile/cardScreen.dart
+++ lib/mainFile/cardScreen.dart
@@ -3,11 +3,7 @@ class cardScreen extends StatelessWidget {
   const cardScreen({super.key});
 
 
- /* cardScreen(
-  {key?key}
-      ):super(key: key)
 
-  */
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
