49141e3cc8369a66128277d45cf9076b3bebbba9
shivamkathait19 <kathaitshivam151@gmail.com>
2025-01-20 21:58:51 +0530

    Update cardScreen.dart

--- lib/mainFile/cardScreen.dart
+++ lib/mainFile/cardScreen.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
 class cardScreen extends StatelessWidget {
 
-  var usernmae,date,father,mother,mobile,email,password;
+  var username,date,father,mother,mobile,email,password;
 
-  cardScreen(
-  {Key?key}
-      required this.username,
-  required this.date
-  required this.father,
-  required this.mother,
-  required this.mobile,
-  required this.email,
-  required this.password,
-
+  cardScreen({Key?key,
+  required this.username,
+    required this.date,
+    required this.father,
+    required this.mother,
+    required this.mobile,
+    required this.email,
+    required this.password,
+  }
 
       ):super(key: key);
 
