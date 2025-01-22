226b198a212cb44479b0b1231af0723988cbf786
shivamkathait19 <kathaitshivam151@gmail.com>
2025-01-22 21:04:04 +0530

    Update MainForm.dart

--- lib/mainFile/MainForm.dart
+++ lib/mainFile/MainForm.dart
@@ -49,182 +49,185 @@ class _mainFormState extends State<mainForm> {
          body:
          Padding(
          padding: const EdgeInsets.all(20.0),
-         child:SingleChildScrollView(
-         child: Form(key: _key,
-           child: Column(
-             children: [
-                  SizedBox(
-                    height: 50,
-                  ),
 
-                       Text("Enter Your Details",style:
+         child: SingleChildScrollView(
 
-                         TextStyle(fontSize: 17,fontStyle: FontStyle.italic)),
-                      SizedBox(
-                        height: 10,
-                      ),
-                     ListTile(
-                       leading: Icon(Icons.person),
-                       title:TextField(
-                         onSubmitted: (input)=>username=input,
-                         decoration: InputDecoration(
-                           labelText: "Username",
-                             labelStyle: TextStyle(
-                               fontStyle: FontStyle.italic,
-                             ),
-                             border: OutlineInputBorder(
-                               borderRadius: BorderRadius.circular(10.10)),
-                         ),
-                         ),
-                     ),
+          child: Form (key: _key,
+             child: Column(
+               children: [
+                    SizedBox(
+                      height: 50,
+                    ),
+
+                         Text("Enter Your Details",style:
+
+                           TextStyle(fontSize: 17,fontStyle: FontStyle.italic)),
                         SizedBox(
-                       height: 10,
-                       ) ,
-                 
-                 ListTile(
-                   leading: Icon(Icons.calendar_today),
-                   title:
-
-                  TextField(
-                    onSubmitted: (input)=>date=input,
-                    decoration: InputDecoration(
-                      labelText: "Date Of Birth",
-                        labelStyle: TextStyle(
-                            fontStyle: FontStyle.italic
-                        ) ,
-
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10.10))),
-                  ),
-                 ),
-                  SizedBox(
-                    height: 10,
-                  ),
-
-                  ListTile(
-                    leading: Icon(Icons.person),
-                    title:
-                    TextField(
-                      onSubmitted: (input)=>father=input,
-                    keyboardType: TextInputType.name,
-                    decoration:InputDecoration(
-                      labelText: "Father Name",
-
-                        labelStyle: TextStyle(
-                          fontStyle: FontStyle.italic
-                        ) ,
-
-                        border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.9)
-                      )
-
-                    ),
-                  ),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.person_2),
-                    title:
-
-                  TextField(
-                    onSubmitted: (input)=>mother=input,
-                    keyboardType: TextInputType.name,
-                    decoration: InputDecoration(
-                      labelText: "Mother Name",
-                      labelStyle: TextStyle(
-                        fontStyle: FontStyle.italic
-                      ),
-
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0)
-                      )
-                    ),
-                  ),
-                  ),
-                  
-                  SizedBox(
-                    height: 10,
-                  ),
-               
-               
-               ListTile(
-                 leading: Icon(Icons.phone),
-                 title:
-
-
-                  TextField(
-                    onSubmitted: (input)=>mobile=input,
-                    keyboardType: TextInputType.number,
-                    inputFormatters: [
-                      FilteringTextInputFormatter.digitsOnly
-                    ],
-                    decoration: InputDecoration(
-                        labelText: "Mobile number",
-                       labelStyle: TextStyle(
-                            fontStyle: FontStyle.italic
-                        ) ,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.9)
-                      )
-                    ),
-                  ),
-               ),
-                  SizedBox(
-                    height: 10,
-                  ),
+                          height: 10,
+                        ),
+                       ListTile(
+                         leading: Icon(Icons.person),
+                         title:TextField(
+                           onSubmitted: (input)=>username=input,
+                           decoration: InputDecoration(
+                             labelText: "Username",
+                               labelStyle: TextStyle(
+                                 fontStyle: FontStyle.italic,
+                               ),
+                               border: OutlineInputBorder(
+                                 borderRadius: BorderRadius.circular(10.10)),
+                           ),
+                           ),
+                       ),
+                          SizedBox(
+                         height: 10,
+                         ) ,
 
                    ListTile(
-                     leading: Icon(Icons.email),
+                     leading: Icon(Icons.calendar_today),
                      title:
 
-                  TextField(
-                    onSubmitted: (input)=>email=input,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      labelText: "Email address",
-                      labelStyle: TextStyle(
-                        fontStyle: FontStyle.italic
-                      ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.10)
+                    TextField(
+                      onSubmitted: (input)=>date=input,
+                      decoration: InputDecoration(
+                        labelText: "Date Of Birth",
+                          labelStyle: TextStyle(
+                              fontStyle: FontStyle.italic
+                          ) ,
 
-                      )
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.10))),
                     ),
-                  ),
                    ),
-                  SizedBox(
-                    height: 10,
-                  ),
+                    SizedBox(
+                      height: 10,
+                    ),
 
-             ListTile(
-               leading: Icon(Icons.password,size: 23,),
-               title:
+                    ListTile(
+                      leading: Icon(Icons.person),
+                      title:
+                      TextField(
+                        onSubmitted: (input)=>father=input,
+                      keyboardType: TextInputType.name,
+                      decoration:InputDecoration(
+                        labelText: "Father Name",
 
-             TextField(
-               onSubmitted: (input)=>password=input,
-               keyboardType: TextInputType.visiblePassword,
+                          labelStyle: TextStyle(
+                            fontStyle: FontStyle.italic
+                          ) ,
 
-               decoration: InputDecoration(
-                 labelText: "Password",
-                 labelStyle: TextStyle(
-                   fontStyle:FontStyle.italic,
+                          border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.9)
+                        )
+
+                      ),
+                    ),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.person_2),
+                      title:
+
+                    TextField(
+                      onSubmitted: (input)=>mother=input,
+                      keyboardType: TextInputType.name,
+                      decoration: InputDecoration(
+                        labelText: "Mother Name",
+                        labelStyle: TextStyle(
+                          fontStyle: FontStyle.italic
+                        ),
+
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0)
+                        )
+                      ),
+                    ),
+                    ),
+
+                    SizedBox(
+                      height: 10,
+                    ),
+
+
+                 ListTile(
+                   leading: Icon(Icons.phone),
+                   title:
+
+
+                    TextField(
+                      onSubmitted: (input)=>mobile=input,
+                      keyboardType: TextInputType.number,
+                      inputFormatters: [
+                        FilteringTextInputFormatter.digitsOnly
+                      ],
+                      decoration: InputDecoration(
+                          labelText: "Mobile number",
+                         labelStyle: TextStyle(
+                              fontStyle: FontStyle.italic
+                          ) ,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.9)
+                        )
+                      ),
+                    ),
+                 ),
+                    SizedBox(
+                      height: 10,
+                    ),
+
+                     ListTile(
+                       leading: Icon(Icons.email),
+                       title:
+
+                    TextField(
+                      onSubmitted: (input)=>email=input,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: "Email address",
+                        labelStyle: TextStyle(
+                          fontStyle: FontStyle.italic
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.10)
+
+                        )
+                      ),
+                    ),
+                     ),
+                    SizedBox(
+                      height: 10,
+                    ),
+
+               ListTile(
+                 leading: Icon(Icons.password,size: 23,),
+                 title:
+
+               TextField(
+                 onSubmitted: (input)=>password=input,
+                 keyboardType: TextInputType.visiblePassword,
+
+                 decoration: InputDecoration(
+                   labelText: "Password",
+                   labelStyle: TextStyle(
+                     fontStyle:FontStyle.italic,
+
+                   ),
+                   border: OutlineInputBorder(
+                     borderRadius: BorderRadius.circular(10.10)
+                   )
 
                  ),
-                 border: OutlineInputBorder(
-                   borderRadius: BorderRadius.circular(10.10)
-                 )
-
+               )
                ),
-             )
+
+                    ButtonTheme(child: ElevatedButton(
+                        onPressed: NextScreen, child: Text("Submit")))
+
+
+               ],
+                 ),
              ),
+         ),
 
-                  ButtonTheme(child: ElevatedButton(
-                      onPressed: NextScreen, child: Text("Submit")))
-
-
-             ],
-               ),
-           ),
-       ),
        )
 
        );
