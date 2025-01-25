a923c53cd27f8d68c6e529b014e063b00acaaa05
shivamkathait19 <kathaitshivam151@gmail.com>
2025-01-25 20:29:37 +0530

    Update cardScreen.dart

--- lib/mainFile/cardScreen.dart
+++ lib/mainFile/cardScreen.dart
@@ -1,4 +1,4 @@
-/*import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 class cardScreen extends StatelessWidget {
 
     String? username;
@@ -72,77 +72,6 @@ class cardScreen extends StatelessWidget {
             ),
     );
 
-  }
-}*/
-import 'package:flutter/material.dart';
-
-class cardScreen extends StatelessWidget {
-  final String? username;
-  final String? date;
-  final String? father;
-  final String? mother;
-  final String? mobile;
-  final String? email;
-  final String? password;
-
-  // Constructor with nullable fields
-  cardScreen({
-    super.key,
-    this.username,
-    this.date,
-    this.father,
-    this.mother,
-    this.mobile,
-    this.email,
-    this.password,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Form(
-          child: Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(70.10),
-            ),
-            child: Column(
-              children: [
-                Padding(padding: EdgeInsets.all(10.0)),
-                ListTile(
-                  leading: Icon(Icons.person),
-                  title: Text(username ?? 'No username provided'),  // Default value if username is null
-                ),
-                ListTile(
-                  leading: Icon(Icons.calendar_today),
-                  title: Text(date ?? 'No date provided'),
-                ),
-                ListTile(
-                  leading: Icon(Icons.person_2),
-                  title: Text(father ?? 'No father name provided'),
-                ),
-                ListTile(
-                  leading: Icon(Icons.person),
-                  title: Text(mother ?? 'No mother name provided'),
-                ),
-                ListTile(
-                  leading: Icon(Icons.phone),
-                  title: Text(mobile ?? 'No mobile number provided'),
-                ),
-                ListTile(
-                  leading: Icon(Icons.email),
-                  title: Text(email ?? 'No email provided'),
-                ),
-                ListTile(
-                  leading: Icon(Icons.password),
-                  title: Text(password ?? 'No password provided'),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
   }
 }
 
