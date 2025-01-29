bc93f397b561eede14638da7e414e85a94941024
shivamkathait19 <kathaitshivam151@gmail.com>
2025-01-29 19:53:39 +0530

    Update cardScreen.dart

--- lib/mainFile/cardScreen.dart
+++ lib/mainFile/cardScreen.dart
@@ -25,57 +25,72 @@ class cardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Form(
-          child: Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30.0),
+
+      body: Padding(
+        padding: EdgeInsets.only(top: 100),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.blue, // Container color
+            borderRadius: BorderRadius.circular(16),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.blue.withOpacity(0.6), // Glow color
+                spreadRadius: 8, // How far the glow extends
+                blurRadius: 20,  // How blurred the glow is
+                offset: Offset(0, 0), // Position of the glow
+              ),
+            ],
+          ),
+           child:SingleChildScrollView(
+          child: Form(
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              child: Column(
+                  children: [
+                Padding(padding: EdgeInsets.all(10.0),),
+
+                ListTile(
+                  leading: Icon(Icons.person),
+                  title: Text(username?? ''),
+
+                ),
+               ListTile(
+               leading: Icon(Icons.calendar_today),
+               title: Text(date ??''),
             ),
-            child: Column(
+                ListTile(
+                  leading: Icon(Icons.person_2),
+                  title: Text(father?? ''),
 
-            children: [
-              Padding(padding: EdgeInsets.all(10.0),),
+                ),
+                ListTile(
+                  leading: Icon(Icons.person),
+                  title: Text(mother?? ''),
+                ),
+                ListTile(
+                  leading: Icon(Icons.phone),
+                  title: Text(mobile??''),
+                ),
+                ListTile(
+                  leading: Icon(Icons.email),
+                  title: Text(email??''),
+                ),
+                ListTile(
+                  leading: Icon(Icons.password),
+                  title: Text(password??''),
+                ),
+              ]
 
-              SizedBox(height: 100,),
-              ListTile(
-                leading: Icon(Icons.person),
-                title: Text(username?? ''),
-
-              ),
-             ListTile(
-             leading: Icon(Icons.calendar_today),
-             title: Text(date ??''),
-          ),
-              ListTile(
-                leading: Icon(Icons.person_2),
-                title: Text(father?? ''),
-
-              ),
-              ListTile(
-                leading: Icon(Icons.person),
-                title: Text(mother?? ''),
-              ),
-              ListTile(
-                leading: Icon(Icons.phone),
-                title: Text(mobile??''),
-              ),
-              ListTile(
-                leading: Icon(Icons.email),
-                title: Text(email??''),
-              ),
-              ListTile(
-                leading: Icon(Icons.password),
-                title: Text(password??''),
-              ),
-            ]
+            ),
 
           ),
-
         ),
-      ),
-            ),
+        ),
+        ),
+      )
     );
-
   }
 }
 
