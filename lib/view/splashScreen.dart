79a6975e76f255020ba9dbe596d08b9aaf0a6264
shivamkathait19 <kathaitshivam151@gmail.com>
2025-02-05 08:31:52 +0530

    Add

--- lib/view/splashScreen.dart
+++ lib/view/splashScreen.dart
@@ -28,12 +28,19 @@ class splashScreen extends StatelessWidget {
                   padding:  EdgeInsets.only(left: 10),
                   child: Container(
                     height: 200,
-                   decoration: BoxDecoration(
-                     border: Border.all(
-                       color: Colors.black54
-                     )
-                   )
-                   ,child: Image.network("https://www.usatoday.com/gcdn/authoring/authoring-images/2024/08/28/USAT/74988734007-debitcredit.jpg?crop=699,393,x0,y0&width=660&height=371&format=pjpg&auto=webp",width: 350, height: 300,)),
+                    decoration: BoxDecoration(
+                      // Container color
+                      //borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black54.withOpacity(0.8), // Glow color
+                          spreadRadius:5, // How far the glow extends
+                          blurRadius:5,  // How blurred the glow is
+                          offset: Offset(0, 0), // Position of the glow
+                        ),
+                      ],
+                    ),
+                   child: Image.network("https://www.usatoday.com/gcdn/authoring/authoring-images/2024/08/28/USAT/74988734007-debitcredit.jpg?crop=699,393,x0,y0&width=660&height=371&format=pjpg&auto=webp",width: 350, height: 300,)),
                 ),
               ],
             ),
@@ -61,9 +68,14 @@ class splashScreen extends StatelessWidget {
                   height: 200,
                   width: 350,
                   decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.black
-                    )
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black54.withOpacity(0.8), // Glow color
+                        spreadRadius:5, // How far the glow extends
+                        blurRadius:2,  // How blurred the glow is
+                        offset: Offset(0, 0), // Position of the glow
+                      ),
+                    ],
                   ),
                   child: Row(
                     children: [Image.network("https://bsmedia.business-standard.com/_media/bs/img/article/2023-03/26/full/1679835161-6589.jpg?im=FeatureCrop,size=(826,465)", width: 347, height: 265,),
