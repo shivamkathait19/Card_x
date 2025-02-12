4665ad5e0f336cd83506cb1af9eb1b7f29444165
shivamkathait19 <kathaitshivam151@gmail.com>
2025-02-12 17:08:41 +0530

    Update LoginScreen.dart

--- lib/view/LoginScreen.dart
+++ lib/view/LoginScreen.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
      LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+
+  String? _email;
+  void _login() {
+    if (_formKey.currentState!.validate()) {
+      // Process login
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Logging in...')));
+      // You can add your logic to handle successful login or navigate to another screen
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,10 +34,25 @@ class LoginScreen extends StatelessWidget {
                 ),
                 SizedBox(
                   height: 20,
-                )
-              Form(child: child)
+                ),
+              Form(
+                key: _formKey,
+                (child: Column (
+                children:[
+                  TextFormField(
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      hintText: 'Enter your email',
+                      prefixIcon: Icon(Icons.email),
+                      border: OutlineInputBorder(),
+                    ),
+                  )
+                ],
+              )
+                ),
               ],
-           )
+           ),
        ),
     );
   }
