0e4c1ba656e7978b1e8e13cb3ead8eba9e106d0a
shivamkathait19 <kathaitshivam151@gmail.com>
2025-02-12 16:51:56 +0530

    Update LoginScreen.dart

--- lib/view/LoginScreen.dart
+++ lib/view/LoginScreen.dart
@@ -9,11 +9,19 @@ class LoginScreen extends StatelessWidget {
            child:Column(
                mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTR90LRqys3ItqiLIzthGpWk-mK_RaKXLHQJddJzObzeFXAhfEFDh1NvshrFGSyLYSNM-o&usqp=CAU",height: 150,
-
+                Padding(
+                  padding: EdgeInsets.only(left: 170, bottom: 10),
+                  child: Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTR90LRqys3ItqiLIzthGpWk-mK_RaKXLHQJddJzObzeFXAhfEFDh1NvshrFGSyLYSNM-o&usqp=CAU",
+                    height: 120,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
                 )
+              Form(child: child)
               ],
-           ) ),
+           )
+       ),
     );
   }
 }
