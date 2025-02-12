2ba2fdfd84a6f3818d4a758ecb4dd93ff7d15fc2
shivamkathait19 <kathaitshivam151@gmail.com>
2025-02-12 16:22:50 +0530

    Add

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:card_x/mainFile/MainForm.dart';
 import 'package:card_x/mainFile/cardScreen.dart';
+import 'package:card_x/view/First.dart';
 import 'package:card_x/view/splashScreen.dart';
 import 'package:flutter/material.dart';
 
@@ -54,7 +55,7 @@ class _MyAppState extends State<MyApp> {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  /*showingsplash ? splashScreen() :*/ (),
+      home:  /*showingsplash ? splashScreen() :*/ firstScreen(),
     );
   }
 }
---
+++ lib/view/First.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class firstScreen extends StatelessWidget {
+  const firstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( backgroundColor: Colors.black,
+      body: Text(""),
+    );
+  }
+}
