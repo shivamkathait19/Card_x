d1e2f9f13c441cf93c545ad4e96a620eeed44c08
shivamkathait19 <kathaitshivam151@gmail.com>
2025-03-09 16:03:44 +0530

    Add

--- lib/view/LoginScreen.dart
+++ lib/view/LoginScreen.dart
@@ -20,151 +20,169 @@ class LoginScreen extends StatefulWidget {
   final TextEditingController _passController = TextEditingController();
   String? _email;
   String? _pass;
+ bool isLoading = false;
+// bool isLoading = false;
 
 //  final AuthService authService = AuthService();
 
   NextScreen() {
     if (_formKey.currentState!.validate()) {}
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (BuildContext context) => mainForm(
-
-        ),
-      ),
-    );
+    setState(() {
+      isLoading = true;
+    });
+    Future.delayed(Duration(seconds: 2),() {
+    setState(() {
+      isLoading = false;
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (BuildContext context) => mainForm())
+      );
+    });
+    });
   }
-  Nextpage() {
+    Nextpage() {
     if (_formKey.currentState!.validate()) {}
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (BuildContext context) => cardScreen(
-        ),
-      ),
-    );
+    setState(() {
+      isLoading = true;
+    });
+    Future.delayed(Duration(seconds: 2), () {
+      setState(() {
+        isLoading = false; // Hide loading indicator
+      });
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (BuildContext context) => cardScreen()),
+      );
+    });
   }
+
 
   @override
        Widget build(BuildContext context) {
-    return Scaffold( backgroundColor: Colors.black12,
+    return Scaffold(backgroundColor: Colors.black12,
       body: Container(
         decoration: BoxDecoration(
-         /* image: DecorationImage(
+          /* image: DecorationImage(
             image: AssetImage('IMG-20241217-WA0037.jpg'),
             fit: BoxFit.cover, // Makes sure the image covers the entire screen
           ),*/
         ),
         child: Padding(
-          padding:  EdgeInsets.only(top: 200),
+          padding: EdgeInsets.only(top: 200),
           child: SafeArea(
-            child : SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: Image.network(
-                    "https://w7.pngwing.com/pngs/178/595/png-transparent-user-profile-computer-icons-login-user-avatars-thumbnail.png",
-                    height: 120,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Image.network(
+                      "https://w7.pngwing.com/pngs/178/595/png-transparent-user-profile-computer-icons-login-user-avatars-thumbnail.png",
+                      height: 120,
+                    ),
                   ),
-                ),
                   SizedBox(
-                  height: 20,
-                   ),
-                // Form widget with correct syntax
-                Form(
-                  key: _formKey,
-                  child: Column(
-                    children: [
-                      // Email TextFormField
-                      Padding(
-                        padding:  EdgeInsets.only(left: 30,right:30),
-                        child: TextFormField(
-                          controller: _emailController,
-                          decoration: InputDecoration(
-                            labelText: 'Email',
-                            labelStyle: TextStyle(color: Colors.grey),
-                            prefixIcon: Icon(Icons.email,color: Colors.grey,) ,
+                    height: 20,
+                  ),
+                  // Form widget with correct syntax
+                  Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        // Email TextFormField
+                        Padding(
+                          padding: EdgeInsets.only(left: 30, right: 30),
+                          child: TextFormField(
+                            controller: _emailController,
+                            decoration: InputDecoration(
+                              labelText: 'Email',
+                              labelStyle: TextStyle(color: Colors.grey),
+                              prefixIcon: Icon(
+                                Icons.email, color: Colors.grey,),
 
-                          ),
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter your email';
-                            }
-                            // Add any additional validation logic here if needed
-                            return null;
-                          },
-                        ),
-                      ),
-          SizedBox(height: 10,),
-                      Padding(
-                        padding: EdgeInsets.only(left: 30,right:30),
-                        child: TextFormField(
-                          controller: _passController,
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            labelText: 'Password',
-                            labelStyle: TextStyle(color: Colors.grey),
-                            prefixIcon: Icon(Icons.password,color: Colors.grey,),
-
-                          ),
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter your Pass';
-                            }
-                            // Add any additional validation logic here if needed
-                            return null;
-                          },
-                        ),
-                      ),
-
-                      Padding(
-                        padding: EdgeInsets.only(left: 220),
-                        child: TextButton(onPressed: (){},
-                          child: Text('Forgot Password',
-                            style: TextStyle(color: Colors.blue,fontSize: 13),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter your email';
+                              }
+                              // Add any additional validation logic here if needed
+                              return null;
+                            },
                           ),
                         ),
-                      ),
-                      // Login Button
-                      Padding(
-                        padding:  EdgeInsets.only(left: 150,right: 150),
-                        child: ElevatedButton(
-                          onPressed: () {
-                            // Only navigate if form is valid
-                            if (_formKey.currentState!.validate()) {
-                              Nextpage();
-                            }
-                          },
-                          child: Text('Login'),
-                          style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(12),
+                        SizedBox(height: 10,),
+                        Padding(
+                          padding: EdgeInsets.only(left: 30, right: 30),
+                          child: TextFormField(
+                            controller: _passController,
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              labelText: 'Password',
+                              labelStyle: TextStyle(color: Colors.grey),
+                              prefixIcon: Icon(
+                                Icons.password, color: Colors.grey,),
+
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter your Pass';
+                              }
+                              // Add any additional validation logic here if needed
+                              return null;
+                            },
+                          ),
+                        ),
+
+                        Padding(
+                          padding: EdgeInsets.only(left: 220),
+                          child: TextButton(onPressed: () {},
+                            child: Text('Forgot Password',
+                              style: TextStyle(
+                                  color: Colors.blue, fontSize: 13),
                             ),
                           ),
                         ),
-                      ),
+                        // Login Button
+                        Padding(
+                          padding: EdgeInsets.only(left: 150, right: 150),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // Only navigate if form is valid
+                              if (_formKey.currentState!.validate()) {
+                                Nextpage();
+                              }
+                            },
 
-                      SizedBox(
-                        height: 10,
-                      ),
-                     SizedBox(height: 200,),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text('Don\'t have an account? ',style: TextStyle(
-                            color: Colors.grey
-                          ),),
-                          TextButton(
-                            onPressed: NextScreen,
+                            child: Text('Login'),
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                            ),
+
+                          ),
+                        ),
+
+                        SizedBox(
+                          height: 10,
+                        ),
+                        SizedBox(height: 200,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('Don\'t have an account? ', style: TextStyle(
+                                color: Colors.grey
+                            ),),
+                            TextButton(
+                              onPressed: NextScreen,
                               // Navigate to the Register screen
-                            child: Text('Sign Up'),
-                          ),
-                          SizedBox(
-                            height: 50,
-                          ),
-                 /*  ElevatedButton(
+                              child: Text('Sign Up'),
+                            ),
+                            SizedBox(
+                              height: 50,
+                            ),
+                            /*  ElevatedButton(
               onPressed: () async {
                 User? user = await authService.signInWithGoogle();
                 if (user != null) {
@@ -175,12 +193,12 @@ class LoginScreen extends StatefulWidget {
             ),*/
 
 
-                        ],
-                      ),
-                    ],
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
               ),
             ),
           ),
@@ -189,4 +207,5 @@ class LoginScreen extends StatefulWidget {
     );
   }
 }
+
 
