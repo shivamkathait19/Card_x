85b146598c62fdf9970cae87b76b64f962c931ce
shivamkathait19 <kathaitshivam151@gmail.com>
2025-03-12 14:41:29 +0530

    Update LoginScreen.dart

--- lib/view/LoginScreen.dart
+++ lib/view/LoginScreen.dart
@@ -13,7 +13,7 @@ class LoginScreen extends StatefulWidget {
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
-   class _LoginScreenState extends State<LoginScreen> {
+class _LoginScreenState extends State<LoginScreen> {
 
   final _formKey = GlobalKey<FormState>();
   final TextEditingController _emailController = TextEditingController();
