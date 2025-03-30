f04bf8e8eb7060901d98d913fca46be480b5b764
shivamkathait19 <kathaitshivam151@gmail.com>
2025-03-30 22:25:41 +0530

    add

--- lib/main.dart
+++ lib/main.dart
@@ -58,7 +58,7 @@ class _MyAppState extends State<MyApp> {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  /*showingsplash ? splashScreen() :*/ SplashScreen(),
+      home:  /*showingsplash ? splashScreen() :*/ LoginScreen(),
 
     );
   }
--- lib/mainFile/cardScreen.dart
+++ lib/mainFile/cardScreen.dart
@@ -1,158 +1,109 @@
 import 'package:flutter/material.dart';
+
 class cardScreen extends StatefulWidget {
-
-
   final String? username;
-     final String? date;
-     final  String? father;
-     final String? mother;
-     final String? mobile;
-     final String? email;
+  final String? date;
+  final String? father;
+  final String? mother;
+  final String? mobile;
+  final String? email;
 
+  const cardScreen({
+    Key? key,
+    this.username,
+    this.date,
+    this.father,
+    this.mother,
+    this.mobile,
+    this.email,
+  }) : super(key: key);
 
-      cardScreen({
-         Key? key,
-        this.username,
-        this.date,
-       this.father,
-       this.mother,
-       this.mobile,
-       this.email,
-
-     }
-  );
-     @override
-  State<cardScreen> createState() => _cardScreenState();
+  @override
+  State<cardScreen> createState() => _CardScreenState();
 }
 
-class _cardScreenState extends State<cardScreen> {
+class _CardScreenState extends State<cardScreen> {
+  void _goToBlankPage() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => BlankPage(
+          username: widget.username,
+          date: widget.date,
+          father: widget.father,
+          mother: widget.mother,
+          mobile: widget.mobile,
+          email: widget.email,
+        ),
+      ),
+    );
+  }
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      body:
-        Padding(
-        padding: EdgeInsets.only(top: 200),
-        child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
-          child: Container(
-            height: 430,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(30),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black54.withOpacity(0.8), // Glow color
-                  spreadRadius:5, // How far the glow extends
-                  blurRadius:5,  // How blurred the glow is
-                  offset: Offset(0, 0), // Position of the glow
-                ),
-              ],
-            ),
-            child:SingleChildScrollView(
-               child: Column(
-                   children: [
-                      SizedBox(
-                    height: 20,
-                  ),
-                      Container(
-                         decoration: BoxDecoration(
-                        border: Border.all(
-                            color: Colors.black54
-                          ),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: ListTile(
-                      leading: Icon(Icons.person),
-                      title: Text(widget.username?? '',),
-                    ),
-                  ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                 Container(
-                   decoration: BoxDecoration(
-                   border: Border.all(
-                   color: Colors.black54,
-                   ),// Container color
-                   borderRadius: BorderRadius.circular(20),
-                   ),
-                   child: ListTile(
-                   leading: Icon(Icons.calendar_today),
-                   title: Text(widget.date ??''),
-                               ),
-                 ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                  Container(decoration: BoxDecoration(
-                    border: Border.all(
-                    color: Colors.black54,
-                  ),// Container color
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-
-                    child: ListTile(
-                      leading: Icon(Icons.person_2),
-                      title: Text(widget.father?? ''),
-
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Container(decoration: BoxDecoration(
-                    border: Border.all(
-                    color: Colors.black54,
-                    ),// Container color
-                     borderRadius: BorderRadius.circular(20),
-
-                  ),
-                    child: ListTile(
-                      leading: Icon(Icons.person),
-                      title: Text(widget.mother?? ''),
-                    ),
-                  ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                  Container(decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.black54,
-                    ),
-                     // Container color
-                    borderRadius: BorderRadius.circular(20),
-
-                  ),
-                    child: ListTile(
-                      leading: Icon(Icons.phone),
-                      title: Text(widget.mobile??''),
-                    ),
-                  ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                  Container(decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.black54,
-                    ),// Container color
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                    child: ListTile(
-                      leading: Icon(Icons.email),
-                      title: Text(widget.email??''),
-                    ),
-                  ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                   ]
-               ),
-            ),
-          ),
-        ),
-        ),
+      body: Center(
+        child: Text("Card Screen"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _goToBlankPage,
+        child: Icon(Icons.person),
+        backgroundColor: Colors.blue,
+      ),
     );
   }
 }
+class BlankPage extends StatelessWidget {
+  final String? username;
+  final String? date;
+  final String? father;
+  final String? mother;
+  final String? mobile;
+  final String? email;
 
+  const BlankPage({
+    Key? key,
+    this.username,
+    this.date,
+    this.father,
+    this.mother,
+    this.mobile,
+    this.email,
+  }) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Blank Page")),
+      body: ListView(
+        padding: EdgeInsets.all(10),
+        children: [
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text(username ?? 'No Name'),
+          ),
+          ListTile(
+            leading: Icon(Icons.calendar_today),
+            title: Text(date ?? 'No Date'),
+          ),
+          ListTile(
+            leading: Icon(Icons.person_2),
+            title: Text(father ?? 'No Father Name'),
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text(mother ?? 'No Mother Name'),
+          ),
+          ListTile(
+            leading: Icon(Icons.phone),
+            title: Text(mobile ?? 'No Mobile'),
+          ),
+          ListTile(
+            leading: Icon(Icons.email),
+            title: Text(email ?? 'No Email'),
+          ),
+        ],
+      ),
+    );
+  }
+}
