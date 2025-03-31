66ffed5d5c0ea5f64ba04b81b4e13957df1bfb96
shivamkathait19 <kathaitshivam151@gmail.com>
2025-03-31 20:13:49 +0530

    Update cardScreen.dart

--- lib/mainFile/cardScreen.dart
+++ lib/mainFile/cardScreen.dart
@@ -41,7 +41,7 @@ class _CardScreenState extends State<cardScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold( backgroundColor: Colors.grey,
       body: Center(
         child: Text("Card Screen"),
       ),
@@ -73,34 +73,54 @@ class BlankPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Blank Page")),
-      body: ListView(
+    return Scaffold( backgroundColor: Colors.grey,
+      appBar: AppBar(title: Text(" PROFILE ")),
+      body:
+      ListView(
         padding: EdgeInsets.all(10),
         children: [
           ListTile(
+
             leading: Icon(Icons.person),
             title: Text(username ?? 'No Name'),
+            titleTextStyle: TextStyle(
+              fontStyle: FontStyle.italic
+            ),
           ),
           ListTile(
             leading: Icon(Icons.calendar_today),
             title: Text(date ?? 'No Date'),
+            titleTextStyle: TextStyle(
+                fontStyle: FontStyle.italic
+            ),
           ),
           ListTile(
             leading: Icon(Icons.person_2),
             title: Text(father ?? 'No Father Name'),
+            titleTextStyle: TextStyle(
+                fontStyle: FontStyle.italic
+            ),
           ),
           ListTile(
             leading: Icon(Icons.person),
             title: Text(mother ?? 'No Mother Name'),
+            titleTextStyle: TextStyle(
+                fontStyle: FontStyle.italic
+            ),
           ),
           ListTile(
             leading: Icon(Icons.phone),
             title: Text(mobile ?? 'No Mobile'),
+            titleTextStyle: TextStyle(
+                fontStyle: FontStyle.italic
+            ),
           ),
           ListTile(
             leading: Icon(Icons.email),
             title: Text(email ?? 'No Email'),
+            titleTextStyle: TextStyle(
+                fontStyle: FontStyle.italic
+            ),
           ),
         ],
       ),
