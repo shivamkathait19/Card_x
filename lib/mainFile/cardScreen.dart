36a0f53c58dc115460f8408f3c8a976a9a8a6a07
shivamkathait19 <kathaitshivam151@gmail.com>
2025-04-27 21:03:38 +0530

    Update cardScreen.dart

--- lib/mainFile/cardScreen.dart
+++ lib/mainFile/cardScreen.dart
@@ -9,7 +9,7 @@ class cardScreen extends StatefulWidget {
   final String? mobile;
   final String? email;
 
-     cardScreen({
+  cardScreen({
     Key? key,
     this.username,
     this.date,
@@ -30,152 +30,155 @@ class _cardScreenState extends State<cardScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(backgroundColor: Colors.black,
-      body: Center(
-        child:Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-          padding:  EdgeInsets.only(bottom:200),
-          child: Image.asset('asset/solated-on-black.jpg',
-            height: 200,
-            width: 150,
-          ),
-        ),
-      Padding(
-        padding: const EdgeInsets.only(left: 30,right: 30),
-  child: ListTile(
-    leading: Icon(Icons.person,),
-    title: Container(
-        decoration: BoxDecoration(
-          border: Border(
-            bottom: BorderSide(
-              color: Colors.white,
-              width: 2.0,
-          ),
-        ),
-        ),
-          child:Center(
-            child: Text(widget.username ?? "Username", style: TextStyle(
-              fontStyle: FontStyle.italic,color: Colors.white
+      body: Padding(
+        padding: const EdgeInsets.only(bottom:200),
+        child: Center(
+          child:Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+            padding:  EdgeInsets.only(bottom:200),
+            child: Image.asset('asset/solated-on-black.jpg',
+              height: 200,
+              width: 150,
             ),
-                  ),
           ),
-      ),
-    )),
+        Padding(
+          padding: const EdgeInsets.only(left: 30,right: 30),
+          child: ListTile(
+            leading: Icon(Icons.person,),
+            title: Container(
+          decoration: BoxDecoration(
+            border: Border(
+              bottom: BorderSide(
+                color: Colors.white,
+                width: 2.0,
+            ),
+          ),
+          ),
+            child:Center(
+              child: Text(widget.username ?? "Username", style: TextStyle(
+                fontStyle: FontStyle.italic,color: Colors.white24
+              ),
+                    ),
+            ),
+        ),
+            )),
 
-            Padding(
-              padding: EdgeInsets.only(left: 30,right: 30),
-              child: ListTile(
-                leading: Icon(Icons.person_2),
-                title:Container(
-                  decoration: BoxDecoration(
-                    border: Border( bottom:
-                      BorderSide(
-                        color: Colors.white,
-                        width: 2.0,
+              Padding(
+                padding: EdgeInsets.only(left: 30,right: 30),
+                child: ListTile(
+                  leading: Icon(Icons.person_2),
+                  title:Container(
+                    decoration: BoxDecoration(
+                      border: Border( bottom:
+                        BorderSide(
+                          color: Colors.white,
+                          width: 2.0,
+                        ),
+
                       ),
 
-                    ),
 
-
-                  ),
-                  child: Center(
-                    child: Text(widget.father ??' Father name',style: TextStyle(
-                        fontStyle: FontStyle.italic,color: Colors.white
-                    ),),
-                  ),
-                ),
-              ),
-            ),
-             Padding(
-               padding: EdgeInsets.only(left: 30,right: 30),
-               child: ListTile(
-                 leading: Icon(Icons.person_2),
-                  title: Container(
-                    decoration: BoxDecoration(
-                      border: Border(bottom:
-                       BorderSide(
-                          color: Colors.white,
-                         width: 2.0,
-                       )
-                      )
                     ),
                     child: Center(
-                      child: Text(widget.mother ?? " Mother name ",style: TextStyle(
-                          fontStyle: FontStyle.italic,color: Colors.white
+                      child: Text(widget.father ??' Father name',style: TextStyle(
+                          fontStyle: FontStyle.italic,color: Colors.white24
                       ),),
                     ),
                   ),
-               ),
-             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 30,right: 30),
-              child: ListTile(
-                leading: Icon(Icons.numbers),
-                title: Container(
-                  decoration: BoxDecoration(
-                      border: Border(bottom:
-                      BorderSide(
-                        color: Colors.white,
-                        width: 2.0,
-                      )
-                      )
-                  ),
-                  child: Center(
-                    child: Text(widget.mother ?? " Mobile number ",style: TextStyle(
-                        fontStyle: FontStyle.italic,color: Colors.white
-                    ),),
-                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 30,right: 30),
-              child: ListTile(
-                leading: Icon(Icons.date_range ,),
-                title: Container(
-                  decoration: BoxDecoration(
-                      border: Border( bottom:
-                      BorderSide(
-                        color: Colors.white,
-                        width: 2.0,
+               Padding(
+                 padding: EdgeInsets.only(left: 30,right: 30),
+                 child: ListTile(
+                   leading: Icon(Icons.person_2),
+                    title: Container(
+                      decoration: BoxDecoration(
+                        border: Border(bottom:
+                         BorderSide(
+                            color: Colors.white,
+                           width: 2.0,
+                         )
+                        )
                       ),
-                      )
+                      child: Center(
+                        child: Text(widget.mother ?? " Mother name ",style: TextStyle(
+                            fontStyle: FontStyle.italic,color: Colors.white24
+                        ),),
+                      ),
+                    ),
+                 ),
+               ),
+              Padding(
+                padding: const EdgeInsets.only(left: 30,right: 30),
+                child: ListTile(
+                  leading: Icon(Icons.numbers),
+                  title: Container(
+                    decoration: BoxDecoration(
+                        border: Border(bottom:
+                        BorderSide(
+                          color: Colors.white,
+                          width: 2.0,
+                        )
+                        )
+                    ),
+                    child: Center(
+                      child: Text(widget.mother ?? " Mobile number ",style: TextStyle(
+                          fontStyle: FontStyle.italic,color: Colors.white24
+                      ),),
+                    ),
                   ),
-                  child: Center(
-                    child:Text(widget.date ??'Date',style: TextStyle(
-                        fontStyle: FontStyle.italic,color: Colors.white
-                    ),),
-                  ),
-
                 ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 30,right: 30),
-              child: ListTile(
-                leading: Icon(Icons.email),
-                title: Container(
-                  decoration: BoxDecoration(
-                      border: Border(bottom:
-                      BorderSide(
-                        color: Colors.white,
-                        width: 2.0,
-                      )
-                      )
+              Padding(
+                padding: EdgeInsets.only(left: 30,right: 30),
+                child: ListTile(
+                  leading: Icon(Icons.date_range ,),
+                  title: Container(
+                    decoration: BoxDecoration(
+                        border: Border( bottom:
+                        BorderSide(
+                          color: Colors.white,
+                          width: 2.0,
+                        ),
+                        )
+                    ),
+                    child: Center(
+                      child:Text(widget.date ??'Date',style: TextStyle(
+                          fontStyle: FontStyle.italic,color: Colors.white24
+                      ),),
+                    ),
+
                   ),
-                  child:  Center(
-                    child: Text(widget.mother ?? " Email  ",style: TextStyle(
-                        fontStyle: FontStyle.italic,color: Colors.white
-                    ),),
-                  )
                 ),
               ),
-            ),
+              Padding(
+                padding: EdgeInsets.only(left: 30,right: 30),
+                child: ListTile(
+                  leading: Icon(Icons.email),
+                  title: Container(
+                    decoration: BoxDecoration(
+                        border: Border(bottom:
+                        BorderSide(
+                          color: Colors.white,
+                          width: 2.0,
+                        )
+                        )
+                    ),
+                    child:  Center(
+                      child: Text(widget.mother ?? " Email  ",style: TextStyle(
+                          fontStyle: FontStyle.italic,color: Colors.white24
+                      ),),
+                    )
+                  ),
+                ),
+              ),
 
-           ],
-        ),
-        ),
+             ],
+          ),
+          ),
+      ),
 
     );
   }
