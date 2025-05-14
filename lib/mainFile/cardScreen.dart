d1afd6978e156fda63ce9520de393660c6a466d0
shivamkathait19 <kathaitshivam151@gmail.com>
2025-05-14 21:28:24 +0530

    Update cardScreen.dart

--- lib/mainFile/cardScreen.dart
+++ lib/mainFile/cardScreen.dart
@@ -90,7 +90,8 @@ class _BlankPageState extends State<BlankPage> {
   }
 
   Widget infoTile(String label, String? value, IconData icon) {
-    return Padding(
+    return  Scaffold(
+        Padding(
       padding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
       child: ListTile(
         leading: Icon(icon, color: Colors.black),
