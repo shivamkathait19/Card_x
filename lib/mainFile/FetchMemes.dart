f89991b4db3f83fe78104b3b78b54b28b2e5145b
shivamkathait19 <kathaitshivam151@gmail.com>
2025-06-02 21:43:43 +0530

    Update FetchMemes.dart

--- lib/mainFile/FetchMemes.dart
+++ lib/mainFile/FetchMemes.dart
@@ -1,11 +1,11 @@
 import 'dart:convert';import 'package:http/http.dart';
 
 
-  class FetchMemes{
-static fetchNewMemes () async{
-Response response =  await get(Uri.parse("https://meme-api.com/gimme"));
-print(response.body);
-Map bodyData = jsonDecode(response.body);
-return (bodyData["url"]);
+   class FetchMemes{
+    static fetchNewMemes () async{
+    Response response =  await get(Uri.parse("https://meme-api.com/gimme"));
+ print(response.body);
+  Map bodyData = jsonDecode(response.body);
+  return (bodyData["url"]);
 }
 }
