289025460bd33fdf9200504bc82c5aab76149f72
shivamkathait19 <kathaitshivam151@gmail.com>
2025-06-11 21:15:03 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -59,7 +59,7 @@ class _MyAppState extends State<MyApp> {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  /*showingsplash ? splashScreen() :*/LoginScreen(),
+      home:  /*showingsplash ? splashScreen() :*/MainForm(),
 
     );
   }
