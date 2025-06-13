d358e630b78e476a43456aa5e8c8492b8839d729
shivamkathait19 <kathaitshivam151@gmail.com>
2025-06-13 21:32:48 +0530

    Update MainForm.dart

--- lib/mainFile/MainForm.dart
+++ lib/mainFile/MainForm.dart
@@ -23,7 +23,7 @@ class _MainFormState extends State<MainForm>{
   bool isloadingdone = false;
   String? selectedGender;
   bool _obscurePassword = true;
-  
+
   List<List<Color>> gradientColors = [
   //  [Colors.teal.shade300, Colors.blue.shade200],
     //[Colors.purple.shade300, Colors.pink.shade200],
@@ -60,6 +60,8 @@ class _MainFormState extends State<MainForm>{
       });
     }
   }
+
+
 
   InputDecoration _inputDecoration(String label){
     return InputDecoration(
