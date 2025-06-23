51d9ef127955411e499a46aae5e47c4403869b0b
shivamkathait19 <kathaitshivam151@gmail.com>
2025-06-23 22:32:18 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'package:card_x/view/splashScreen.dart';
 //import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+
+
 void main() /*async*/ {
 //WidgetsFlutterBinding.ensureInitialized();
 //await Firebase.initializeApp();
