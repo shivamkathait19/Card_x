a40ec174d001d5021e0c940a214cfb95a724f26c
shivamkathait19 <kathaitshivam151@gmail.com>
2025-07-19 22:51:03 +0530

    Create Barcoms.dart

---
+++ lib/mainFile/Barcoms.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class Barcoms extends StatelessWidget {
+   Barcoms({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
