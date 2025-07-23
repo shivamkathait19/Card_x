1dc3cc641b6f3527d21ea6f5c2d9c60fd04b3264
shivamkathait19 <kathaitshivam151@gmail.com>
2025-07-23 20:13:35 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:card_x/mainFile/Barcoms.dart';
 import  'package:card_x/mainFile/MainForm.dart';
-
+import 'package:card_x/';
 import 'package:card_x/mainFile/cardScreen.dart';
 import 'package:card_x/view/LoginScreen.dart';
 import 'package:card_x/view/splashScreen.dart';
