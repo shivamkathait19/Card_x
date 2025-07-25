c748592118ad704e746dae86579fe2da047789bf
shivamkathait19 <kathaitshivam151@gmail.com>
2025-07-25 22:00:57 +0530

    Add

--- lib/mainFile/MakeScreen.dart
+++ lib/mainFile/MakeScreen.dart
@@ -1,45 +1,53 @@
 import 'package:flutter/material.dart';
 
-class Makescreen extends StatelessWidget {
-  const Makescreen({super.key});
+class Makescreen extends StatelessWidget implements PreferredSizeWidget {
+  Makescreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Form(
-          child: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.only(top: 100,right: 20,left: 20),
-                child: TextFormField(
-                  style: TextStyle(color: Colors.black),
-                  decoration: InputDecoration(
-                    labelText: "Enter the location name "
-                  ),
-                ),
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 2,
+      leading: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: CircleAvatar(
+          backgroundImage: AssetImage("assets/logo.png",) // Your logo
+        ),
+      ),
+      title: RichText(
+        text:  TextSpan(
+          children: [
+            TextSpan(
+              text: 'shivam',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.deepPurple,
               ),
-SizedBox(
-  height: 20,
-),
-              Padding(
-                padding:  EdgeInsets.only(top: 20,right: 20,left: 20),
-                child: TextFormField(
-                  style: TextStyle(color: Colors.black),
-                  decoration: InputDecoration(
-                    labelText: 'ENTER THE PRICE '
-                  ),
-                ),
-              )
-            ],
-          ),
-        )
-      )
-      );
-
+            ),
+            TextSpan(
+              text: 'Trip',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.pinkAccent,
+              ),
+            ),
+          ],
+        ),
+      ),
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.menu, color: Colors.black87),
+          onPressed: () {
+            Scaffold.of(context).openEndDrawer(); // Optional drawer trigger
+          },
+        ),
+      ],
+    );
   }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
-/*child: Form(
-          key: _formKey,
-          child: Column(
-            children: [*/
+
