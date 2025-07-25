d995b219d2a0ddf21b0e0b67e56cdc67948c0b8e
shivamkathait19 <kathaitshivam151@gmail.com>
2025-07-25 23:43:04 +0530

    Add

--- lib/mainFile/MakeScreen.dart
+++ lib/mainFile/MakeScreen.dart
@@ -1,40 +1,41 @@
 import 'package:flutter/material.dart';
 
-class Makescreen extends StatelessWidget implements PreferredSizeWidget {
+class Makescreen extends StatelessWidget  {
   Makescreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      backgroundColor: Colors.white,
+    return Scaffold(
+    appBar:   AppBar(
+      backgroundColor: Colors.white10,
       elevation: 2,
       leading: Padding(
         padding: const EdgeInsets.all(8.0),
         child: CircleAvatar(
-          backgroundImage: AssetImage("assets/logo.png",) // Your logo
+            backgroundImage: AssetImage("",) // Your logo
         ),
       ),
-      title: RichText(
-        text:  TextSpan(
-          children: [
-            TextSpan(
-              text: 'shivam',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.deepPurple,
-              ),
+      title: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: const [
+          Text(
+            'Make',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.deepPurple,
             ),
-            TextSpan(
-              text: 'Trip',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.pinkAccent,
-              ),
+          ),
+          Text(
+            'Card',
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w500,
+              color: Colors.pinkAccent,
             ),
-          ],
-        ),
+          ),
+        ],
       ),
       actions: [
         IconButton(
@@ -44,10 +45,43 @@ class Makescreen extends StatelessWidget implements PreferredSizeWidget {
           },
         ),
       ],
-    );
-  }
+    ),
 
-  @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+      body: Padding(
+        padding:  EdgeInsets.all(15.0),
+        child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.5),
+              border: Border.all(
+                color: Colors.black12.withOpacity(0.5), // Change to your preferred border color
+                width: 1.0,
+              ),
+              borderRadius: BorderRadius.circular(25),
+            ),
+            child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    labelText: 'Location',
+                    labelStyle: TextStyle(
+                    color: Colors.black,
+                        fontStyle: FontStyle.italic
+                ),
+              border: OutlineInputBorder(borderRadius: BorderRadius.circular(10),
+              ),
+
+                        ),
+              
+                      ),
+
+            ),
+
+          )
+
+        ),
+      ));
+
+  }
 }
 
