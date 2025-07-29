7f82db0ad6560ce6f964dfe37ec11f57db0deec9
shivamkathait19 <kathaitshivam151@gmail.com>
2025-07-29 13:37:18 +0530

    Update MakeScreen.dart

--- lib/mainFile/MakeScreen.dart
+++ lib/mainFile/MakeScreen.dart
@@ -1,59 +1,66 @@
-import 'package:card_x/mainFile/cardScreen.dart';
 import 'package:flutter/material.dart';
 
-class Makescreen extends StatelessWidget  {
+class Makescreen extends StatelessWidget {
   Makescreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.brown,
-      appBar:  AppBar(
+      appBar: AppBar(
         backgroundColor: Colors.white10,
-      elevation: 5,
-      leading: Padding(
-        padding:  EdgeInsets.only(left: 30),
-        child: CircleAvatar(
-          radius: 1,
-            backgroundImage: AssetImage("asset/avatar-png.jpg") // Your logo
+        elevation: 5,
+        leadingWidth: 100,
+        leading: Builder(
+          builder: (context) => Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              IconButton(
+                icon: Icon(Icons.menu, color: Colors.white),
+                onPressed: () => Scaffold.of(context).openDrawer(),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 0),
+                child: CircleAvatar(
+                  radius: 16,
+                  backgroundImage: AssetImage("assets/avatar-png.jpg"), // Check path
+                ),
+              ),
+            ],
+          ),
+        ),
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: const [
+            Text(
+              'Make',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.deepPurple,
+              ),
+            ),
+            Text(
+              'Card',
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w500,
+                color: Colors.pinkAccent,
+              ),
+            ),
+          ],
         ),
       ),
-      title: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: const [
-          Text(
-            'Make',
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-              color: Colors.deepPurple,
-            ),
-          ),
-          Text(
-            'Card',
-            style: TextStyle(
-              fontSize: 14,
-              fontWeight: FontWeight.w500,
-              color: Colors.pinkAccent,
-            ),
-          ),
-        ],
-      ),
-      actions: [
-        ElevatedButton(onPressed: onPressed, child: child)
-      ],
-      ),
-      
-      
+
       drawer: Drawer(
         backgroundColor: Colors.black,
         child: ListView(
-         // padding: EdgeInsets.zero,
           children: [
             DrawerHeader(
               decoration: BoxDecoration(
-                gradient: LinearGradient(colors: [Colors.deepPurple, Colors.pinkAccent]),
+                gradient: LinearGradient(
+                    colors: [Colors.deepPurple, Colors.pinkAccent]),
               ),
               child: Text(
                 'Menu',
@@ -61,27 +68,24 @@ class Makescreen extends StatelessWidget  {
               ),
             ),
             ListTile(
-              leading: Icon(Icons.home),
-              title: Text('Home'),
+              leading: Icon(Icons.home, color: Colors.white),
+              title: Text('Home', style: TextStyle(color: Colors.white)),
               onTap: () {
                 Navigator.pop(context);
-                // Handle navigation
               },
             ),
             ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('Settings'),
+              leading: Icon(Icons.settings, color: Colors.white),
+              title: Text('Settings', style: TextStyle(color: Colors.white)),
               onTap: () {
                 Navigator.pop(context);
-                // Handle navigation
               },
             ),
             ListTile(
-              leading: Icon(Icons.info),
-              title: Text('About'),
+              leading: Icon(Icons.info, color: Colors.white),
+              title: Text('About', style: TextStyle(color: Colors.white)),
               onTap: () {
                 Navigator.pop(context);
-                // Handle navigation
               },
             ),
           ],
@@ -99,40 +103,40 @@ class Makescreen extends StatelessWidget  {
             ),
             borderRadius: BorderRadius.circular(25),
           ),
-          padding:  EdgeInsets.all(16),
+          padding: EdgeInsets.all(16),
           child: SingleChildScrollView(
             child: Column(
               children: [
-                Image.network("https://t3.ftcdn.net/jpg/05/92/76/32/360_F_592763239_V1Bj5YHCIRHreEfYRFwIcVaRBEqcCt1i.jpg",
-               fit: BoxFit.cover,
+                Image.network(
+                  "https://t3.ftcdn.net/jpg/05/92/76/32/360_F_592763239_V1Bj5YHCIRHreEfYRFwIcVaRBEqcCt1i.jpg",
+                  fit: BoxFit.cover,
                 ),
-                SizedBox(height: 19,),
+                SizedBox(height: 19),
                 _buildField('Text'),
-                const SizedBox(height: 16),
+                SizedBox(height: 16),
                 _buildField('Location'),
-                const SizedBox(height: 16),
+                SizedBox(height: 16),
                 _buildField('Duration'),
-                const SizedBox(height: 16),
-                _buildField('People'),
-                const SizedBox(height: 16),
-                _buildField('Description', maxLines:1),
-                const SizedBox(height: 24),
-                //_buildField("Book now "),
-               // ElevatedButton(onPressed: (){}, child: Text("Just 89999",style: TextStyle(fontSize: 20,fontStyle: FontStyle.italic),)),
-               //SizedBox(height:''),
+                SizedBox(height: 16),
+                _buildField('People',),
+                SizedBox(height: 16),
+                _buildField('Description', maxLines: 1),
+                SizedBox(height: 24),
                 ElevatedButton.icon(
                   onPressed: () {
                     // Submit action or Firebase logic
                   },
-                  icon: const Icon(Icons.add),
-                  label:  Text(" JUST 8999 BOOK NOW ",style: TextStyle(color: Colors.black),),
+                  icon: Icon(Icons.add, color: Colors.black),
+                  label: Text(
+                    " JUST 8999 BOOK NOW ",
+                    style: TextStyle(color: Colors.black),
+                  ),
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.blue.withOpacity(0.6),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(12),
                     ),
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 30, vertical: 14),
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 14),
                   ),
                 ),
               ],
@@ -142,16 +146,18 @@ class Makescreen extends StatelessWidget  {
       ),
     );
   }
-   Widget _buildField(String label, {int maxLines = 1}) {
+
+  Widget _buildField(String label, {int maxLines = 1}) {
     return TextFormField(
       maxLines: maxLines,
+      keyboardType: TextInputType.number,
+      // inputFormatters: [
+      //   FilteringTextInputFormatter.digitsOnly
+      // ],
       decoration: InputDecoration(
         labelText: label,
-        labelStyle: const TextStyle(
-          color: Colors.black,
-          fontStyle: FontStyle.italic,
-          fontSize: 15
-        ),
+        labelStyle: TextStyle(
+            color: Colors.black, fontStyle: FontStyle.italic, fontSize: 15),
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(10),
         ),
@@ -159,4 +165,3 @@ class Makescreen extends StatelessWidget  {
     );
   }
 }
-
