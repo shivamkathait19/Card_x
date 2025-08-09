ec4dcbf2fdbdefb3b4a72931bf8d074a840ef741
shivamkathait19 <kathaitshivam151@gmail.com>
2025-08-09 10:18:19 +0530

    Update Barcoms.dart

--- lib/mainFile/Barcoms.dart
+++ lib/mainFile/Barcoms.dart
@@ -22,6 +22,8 @@ class _BarcomsState extends State<Barcoms> {
    Cardmaker(){
     Navigator.push(context, MaterialPageRoute(builder: (BuildContext Context)=>Card()));
    }
+   
+   
   @override
   Widget build(BuildContext context) {
     return Scaffold(backgroundColor: Colors.brown.shade50,
